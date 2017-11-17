41b624ff9f0c73b1821647e413958a8e281ec63a
angelinn <angelin.nedelchev@outlook.com>
2017-11-17 22:21:12 +0200

    Add comment and ignore attributes.

---
+++ src/Fluffy.Ini/Fluffy.Ini/Attributes/FluffyComment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluffy.Ini.Attributes
+{
+    public class FluffyComment : Attribute
+    {
+        private string content;
+
+        public FluffyComment(string content)
+        {
+            this.content = content;
+        }
+    }
+}
---
+++ src/Fluffy.Ini/Fluffy.Ini/Attributes/FluffyIgnore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluffy.Ini.Attributes
+{
+    public class FluffyIgnore : Attribute
+    {
+    }
+}
