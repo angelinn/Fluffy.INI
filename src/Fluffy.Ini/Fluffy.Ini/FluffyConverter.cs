b977a0d0873b71125a1401cd83aedf35b5d703c5
angelinn <angelin.nedelchev@outlook.com>
2017-11-17 21:44:50 +0200

    Add base FluffyConverter class.

---
+++ src/Fluffy.Ini/Fluffy.Ini/FluffyConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluffy.Ini
+{
+    public static class FluffyConverter
+    {
+        public static string SerializeObject<T>(T target) where T : class
+        {
+            return String.Empty;
+        }
+
+        public static T DeserializeObject<T>(string iniContent) where T : class
+        {
+            return null;
+        }
+    }
+}
