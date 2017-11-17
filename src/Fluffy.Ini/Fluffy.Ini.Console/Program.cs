1a4b27156b1406311b48d4e950b2f434236a5640
angelinn <angelin.nedelchev@outlook.com>
2017-11-17 22:08:37 +0200

    Add test console app.

---
+++ src/Fluffy.Ini/Fluffy.Ini.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using Fluffy.Ini.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluffy.Ini.Console
+{
+    public class Settings
+    {
+        public int Volume { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class IniFile
+    {
+        [FluffyIgnore]
+        public string Wut { get; set; }
+        public Settings Settings { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ini = FluffyConverter.SerializeObject(new IniFile
+            {
+                Wut = "hi",
+                Settings = new Settings
+                {
+                    Name = "Wow Settings!",
+                    Volume = 99
+                }
+            });
+        }
+    }
+}
