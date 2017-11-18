6bc01c7dec8893d82f48a7178f3c7d85bb5e823f
angelinn <angelin.nedelchev@outlook.com>
2017-11-18 15:08:58 +0200

    Add unit test project.

---
+++ src/Fluffy.Ini/Fluffy.Ini.Tests/FluffyConverterTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluffy.Ini.Tests
+{
+    [TestFixture]
+    public class FluffyConverterTests
+    {
+        [Test]
+        public void SerializeOneLevelObject()
+        {
+            Assert.True(true);
+        }
+    }
+}
